2260f3bbea65c7afab79041e018377574d285376
Ramy Aboul Naga <ramy.naga@gmail.com>
2021-04-05 17:40:58 +0200

    Bootstrap gem

---
+++ lib/rubernetes.rb
@@ -0,0 +1,5 @@
+require "rubernetes/version"
+
+module Rubernetes
+  class Error < StandardError; end
+end
---
+++ spec/rubernetes_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Rubernetes do
+  it "has a version number" do
+    expect(Rubernetes::VERSION).not_to be_nil
+  end
+end
