c8a0ab084ca45ac8fdd27b6767730d3c7de7e3a6
Alaa <soupermado@gmail.com>
2023-03-01 00:00:45 +0200

    [Fix] rubocop

--- spec/rubernetes/operator_spec.rb
+++ spec/rubernetes/operator_spec.rb
@@ -1,30 +1,37 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe Rubernetes::Operator do
   describe '#initialize' do
     context 'with default options' do
-      subject { described_class.new('test', 'v1', 'tests', {}) }
+      subject(:test_operator) { described_class.new('test', 'v1', 'tests', {}) }
 
-      it 'should create a YAML store' do
+      it 'creates a YAML store' do
         expect(YAML::Store).to receive(:new).with('/tmp/cache/test_v1_tests.yaml')
-        subject
+        test_operator
       end
 
-      it 'should set default options' do
-        expect(subject.instance_variable_get(:@options)).to eq(sleepTimer: 1, namespace: nil, persistence_location: '/tmp/cache')
+      it 'sets default options' do
+        expect(test_operator.instance_variable_get(:@options)).to eq(sleepTimer: 1, namespace: nil,
+                                                                     persistence_location: '/tmp/cache')
       end
     end
 
     context 'with custom options' do
-      subject { described_class.new('test', 'v1', 'tests', { sleepTimer: 5, namespace: 'mynamespace', persistence_location: '/tmp/custom' }) }
-
-      it 'should create a YAML store' do
-        expect(YAML::Store).to receive(:new).with('/tmp/custom/test_v1_tests.yaml')
-        subject
+      subject(:test_operator) do
+        described_class.new('test', 'v1', 'tests',
+                            { sleepTimer: 5, namespace: 'mynamespace', persistence_location: '/tmp/custom' })
       end
 
-      it 'should set custom options' do
-        expect(subject.instance_variable_get(:@options)).to eq(sleepTimer: 5, namespace: 'mynamespace', persistence_location: '/tmp/custom')
+      it 'creates a YAML store' do
+        expect(YAML::Store).to receive(:new).with('/tmp/custom/test_v1_tests.yaml')
+        test_operator
+      end
+
+      it 'sets custom options' do
+        expect(test_operator.instance_variable_get(:@options)).to eq(sleepTimer: 5, namespace: 'mynamespace',
+                                                                     persistence_location: '/tmp/custom')
       end
     end
   end
